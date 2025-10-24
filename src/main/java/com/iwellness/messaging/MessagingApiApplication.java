069677c2698755e6a8a634133374f70f41bb3aba
Christian Santiago Vera Rojas <104735286+SantiagoVera9812@users.noreply.github.com>
2025-10-24 17:50:35 -0500

    revisar JWT

--- src/main/java/com/iwellness/messaging/MessagingApiApplication.java
+++ src/main/java/com/iwellness/messaging/MessagingApiApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableFeignClients
 public class MessagingApiApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
         SpringApplication.run(MessagingApiApplication.class, args);
     }
 }
